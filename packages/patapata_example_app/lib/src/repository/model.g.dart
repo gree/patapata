97771d7973c82f3d82cbe52d9512f7a78afc398d
matsuyani556 <69038535+matsuyani556@users.noreply.github.com>
2025-02-10 16:42:22 +0900

    add license header

--- packages/patapata_example_app/lib/src/repository/model.g.dart
+++ packages/patapata_example_app/lib/src/repository/model.g.dart
@@ -1,3 +1,8 @@
+// Copyright (c) GREE, Inc.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'model.dart';
