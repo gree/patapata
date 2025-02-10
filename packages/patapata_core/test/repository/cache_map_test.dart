896d53b32d6e212da2da9197299a025cc6403245
matsuyani556 <69038535+matsuyani556@users.noreply.github.com>
2025-02-10 16:45:14 +0900

    add license header

--- packages/patapata_core/test/repository/cache_map_test.dart
+++ packages/patapata_core/test/repository/cache_map_test.dart
@@ -1,3 +1,8 @@
+// Copyright (c) GREE, Inc.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import 'package:test/test.dart';
 import 'package:patapata_core/src/repository.dart';
 
