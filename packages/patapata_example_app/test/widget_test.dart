85d165bd015c45de1110a89e65582d5613791ada
matsuyani556 <69038535+matsuyani556@users.noreply.github.com>
2025-03-28 09:41:05 +0900

    add license header

--- packages/patapata_example_app/test/widget_test.dart
+++ packages/patapata_example_app/test/widget_test.dart
@@ -1,3 +1,8 @@
+// Copyright (c) GREE, Inc.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
